332e550c53a5daab65c6d27debad752a384426d1
Carlitos <carlos.iriarte@ucb.edu.bo>
2025-09-06 18:28:18 -0400

    feat(score): actualizar marcador a 15-Love cuando el Jugador 1 anota

--- tests/score.test.js
+++ tests/score.test.js
@@ -5,5 +5,11 @@ describe("Tennis Score", () => {
     let score = "Love-Love";  // Inicializamos el marcador
     expect(score).toBe("Love-Love");  // Comprobamos el marcador es Love-Love
   });
+
+  it("Marcador cambia a 15-Love cuando jugador 1 anota", () => {
+    let score = "Love-Love";  // Marcador inicial
+    score = "15-Love";  // Jugador 1 anota
+    expect(score).toBe("15-Love");  // Verificamos que el marcador es 15-Love
+  });
 });
 
